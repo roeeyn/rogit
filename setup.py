8a241fdc22d8409035e0ddd086b488d622875db0
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-06-09 16:44:18 -0500

    Modified dist info

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+version = "1.3"
 
 setup(
     name="rogit",
-    version="1.0",
+    version=version,
     license="MIT",
     description="This is a little git automation for lazy devs. Add, commit and push in one single command.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="roeeyn",
     author_email="rodrigo.medina.neri@gmail.com",
     url="https://github.com/roeeyn/rogit",
-    download_url="https://github.com/roeeyn/rogit/archive/v_01.tar.gz",
+    download_url=f"https://github.com/roeeyn/rogit/releases/download/{version}/rogit-{version}.tar.gz",
     keywords=["git", "automation"],
     py_modules=["app"],
     install_requires=["Click",],
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 5 - Production/Stable",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",  # Again, pick a license
